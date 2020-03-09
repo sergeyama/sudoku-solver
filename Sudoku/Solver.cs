371a570e9e3b4f0397cbf7bbcb04abe87c8bca39
Sergei <sergeyama@mail.ru>
2020-03-09 13:42:41 +0300

    Solver - добавлен класс для решения головоломки

---
+++ Sudoku/Solver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Sudoku
+{
+    public class Solver
+    {
+        private Grid _grid;
+
+        public Solver(Grid grid)
+        {
+            _grid = grid;
+        }
+
+        public void SolveIt()
+        {
+            foreach (Cell item in _grid)
+            {
+                if (item.IsActive)
+                {
+                    var groupCells = _grid.GetGroupCells(item.X, item.Y, item.R);
+
+                    foreach (Cell cell in groupCells)
+                    {
+                        if (!cell.IsActive)
+                            item.PossibleValues.Remove(cell.Value);
+                    }                    
+                }
+            }
+
+            foreach (Cell item in _grid)
+            {
+                if (item.IsActive)
+                {
+                    var groupCells = _grid.GetGroupCells(item.X, item.Y, item.R);
+                    groupCells = groupCells.Where(o => o.R == item.R && o.PossibleValues != null).ToList();
+                    int hasMatch = 0;
+
+                    foreach (int number in item.PossibleValues)
+                    {
+                        foreach (Cell cell in groupCells)
+                        {
+                            if (cell.PossibleValues.Contains(number))
+                                hasMatch++;
+                        }
+
+                        if (hasMatch == 0)
+                        {
+                            item.Value = number;
+                            item.PossibleValues = null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
