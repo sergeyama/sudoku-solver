831b54cb64294e7edea3dbcd1370b49812077ee6
Sergei <sergeyama@mail.ru>
2020-03-09 10:09:41 +0300

    добавлена структура Cell для хранения состояний ячейки

---
+++ Sudoku/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku
+{
+    public struct Cell
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public bool IsActive { get; }
+        public List<int> PossibleValues { get; set; }
+
+        public Cell(string name, int value, bool isActive)
+        {
+            Name = name;            
+            Value = value;
+            IsActive = isActive;
+            PossibleValues = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            if (Value != 0)
+                PossibleValues.Remove(Value);
+        }
+    }
+}
