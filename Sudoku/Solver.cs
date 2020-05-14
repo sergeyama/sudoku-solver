2833a5c8ae7f6cd53ce6f428408f120ac0304f40
Sergei <sergeyama@mail.ru>
2020-05-14 23:38:05 +0300

    переработан алгоритм, готово полное решение простых и средних головоломок

--- Sudoku/Solver.cs
+++ Sudoku/Solver.cs
@@ -14,6 +14,10 @@ namespace Sudoku
         /// Игровое поле.
         /// </summary>
         private Grid _grid;
+        /// <summary>
+        /// Признак найденной уникальной цифры.
+        /// </summary>
+        private bool _findNumber = true;
 
         /// <summary>
         /// Создание решения для конкретного поля.
@@ -29,8 +33,54 @@ namespace Sudoku
         /// </summary>
         public void SolveIt()
         {
+            //Убираем дубли начальных значений из возможных среди пустых ячеек.
             SetPossibleValues();
 
+            //Выставляем очевидные значения.
+            FindUniqueValueAlgorithm();
+
+            while (_findNumber)
+            {
+                _findNumber = false;
+
+                FindTwoCellsWithTwoPairAlgorithm();
+
+                FindUniqueValueAlgorithm();
+            }
+        }
+
+        private void FindTwoCellsWithTwoPairAlgorithm()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                var rectungleCells = _grid.GetRectungleGroupCells(i + 1).Where(o => o.IsActive).ToList();
+
+                foreach (var cell in rectungleCells)
+                {
+                    List<Cell> twoCells = rectungleCells.Where(o => o.PossibleValues.SequenceEqual(cell.PossibleValues) && o.PossibleValues.Count == 2).ToList();
+
+                    if (twoCells.Count == 2)
+                    {
+                        foreach (var cellForRemove in rectungleCells)
+                        {
+                            if (!cellForRemove.PossibleValues.SequenceEqual(twoCells[0].PossibleValues))
+                            {
+                                cellForRemove.PossibleValues.Remove(twoCells[0].PossibleValues[0]);
+                                cellForRemove.PossibleValues.Remove(twoCells[0].PossibleValues[1]);
+
+                                //TODO: добавить чистку дубликатов из строк и стобцов для подобных ячеек
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Поиск уникального значения в ячейке среди смежных ячеек и установка значений с деактивацией.
+        /// </summary>
+        private void FindUniqueValueAlgorithm()
+        {          
             foreach (Cell item in _grid)
             {
                 if (item.IsActive)
@@ -49,19 +99,21 @@ namespace Sudoku
                                 hasMatch++;
                         }
 
-                        if (hasMatch == 0)
+                        if (hasMatch == 0 || (item.IsActive && item.PossibleValues.Count == 1))
                         {
                             item.Value = number;
                             item.PossibleValues = null;
                             item.IsActive = false;
+                            _findNumber = true;
+                            SetPossibleValues();
                         }
                     }
                 }
-            }
+            }            
         }
 
         /// <summary>
-        /// Убирает дубликаты значений связанных ячеек в списке возможных значений всех ячеек.
+        /// Убирает дубликаты значений связанных ячеек (не активных) в списке возможных значений всех ячеек.
         /// </summary>
         private void SetPossibleValues()
         {
